d02aad8460097e50813a48d3048757f9fdb2c8d6
Slava Tumanov <vn.tumanov@me.com>
2020-10-07 23:24:13 +0300

    add attachments @Test

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -10,32 +10,33 @@ class WallServiceTest {
     @Test
     fun addPost() {
         val result = service.add(
-            Post(
-                ownerId = 0,
-                fromId = 0,
-                createdBy = 0,
-                date = 1601510399,
-                text = "this text post",
-                replyOwnerId = 0,
-                replyPostId = 0,
-                friendsOnly = true,
-                comments = Comments(count = 1, canPost = true, groupCanPost = true, canClose = false, canOpen = false),
-                copyright = "copyright1",
-                likes = Likes(count = 1, userLikes = true, canLike = false, canPublish = false),
-                reposts = Reposts(count = 1, userReposted = false),
-                views = Views(1),
-                postType = "post",
-                postSource = null,
-                geo = null,
-                signerId = 0,
-                canPin = true,
-                canDelete = true,
-                canEdit = true,
-                isPinned = true,
-                markedAsAds = true,
-                isFavorite = true,
-                postponedId = 1
-            )
+                Post(
+                        ownerId = 0,
+                        fromId = 0,
+                        createdBy = 0,
+                        date = 1601510399,
+                        text = "this text post",
+                        replyOwnerId = 0,
+                        replyPostId = 0,
+                        friendsOnly = true,
+                        comments = Comments(count = 1, canPost = true, groupCanPost = true, canClose = false, canOpen = false),
+                        copyright = "copyright1",
+                        likes = Likes(count = 1, userLikes = true, canLike = false, canPublish = false),
+                        reposts = Reposts(count = 1, userReposted = false),
+                        views = Views(1),
+                        postType = "post",
+                        postSource = null,
+                        attachments = null,
+                        geo = null,
+                        signerId = 0,
+                        canPin = true,
+                        canDelete = true,
+                        canEdit = true,
+                        isPinned = true,
+                        markedAsAds = true,
+                        isFavorite = true,
+                        postponedId = 1
+                )
         )
         assertEquals(result.id, 1)
     }
@@ -43,12 +44,42 @@ class WallServiceTest {
     @Test
     fun updateExistingTrue() {
         service.add(
-            Post(
+                Post(
+                        ownerId = 0,
+                        fromId = 0,
+                        createdBy = 0,
+                        date = 1601510399,
+                        text = "this text post",
+                        replyOwnerId = 0,
+                        replyPostId = 0,
+                        friendsOnly = true,
+                        comments = Comments(count = 1, canPost = true, groupCanPost = true, canClose = false, canOpen = false),
+                        copyright = "copyright1",
+                        likes = Likes(count = 1, userLikes = true, canLike = false, canPublish = false),
+                        reposts = Reposts(count = 1, userReposted = false),
+                        views = Views(1),
+                        postType = "post",
+                        postSource = null,
+                        attachments = null,
+                        geo = null,
+                        signerId = 0,
+                        canPin = true,
+                        canDelete = true,
+                        canEdit = true,
+                        isPinned = true,
+                        markedAsAds = true,
+                        isFavorite = true,
+                        postponedId = 1
+                )
+        )
+
+        val updatePost = Post(
+                id = 1,
                 ownerId = 0,
                 fromId = 0,
                 createdBy = 0,
-                date = 1601510399,
-                text = "this text post",
+                date = 1601510500,
+                text = "this update text post",
                 replyOwnerId = 0,
                 replyPostId = 0,
                 friendsOnly = true,
@@ -59,6 +90,7 @@ class WallServiceTest {
                 views = Views(1),
                 postType = "post",
                 postSource = null,
+                attachments = null,
                 geo = null,
                 signerId = 0,
                 canPin = true,
@@ -68,35 +100,6 @@ class WallServiceTest {
                 markedAsAds = true,
                 isFavorite = true,
                 postponedId = 1
-            )
-        )
-
-        val updatePost = Post(
-            id = 1,
-            ownerId = 0,
-            fromId = 0,
-            createdBy = 0,
-            date = 1601510500,
-            text = "this update text post",
-            replyOwnerId = 0,
-            replyPostId = 0,
-            friendsOnly = true,
-            comments = Comments(count = 1, canPost = true, groupCanPost = true, canClose = false, canOpen = false),
-            copyright = "copyright1",
-            likes = Likes(count = 1, userLikes = true, canLike = false, canPublish = false),
-            reposts = Reposts(count = 1, userReposted = false),
-            views = Views(1),
-            postType = "post",
-            postSource = null,
-            geo = null,
-            signerId = 0,
-            canPin = true,
-            canDelete = true,
-            canEdit = true,
-            isPinned = true,
-            markedAsAds = true,
-            isFavorite = true,
-            postponedId = 1
         )
         val result = service.update(updatePost)
         assertTrue("true", result)
@@ -105,12 +108,42 @@ class WallServiceTest {
     @Test
     fun updateExistingFalse() {
         service.add(
-            Post(
+                Post(
+                        ownerId = 0,
+                        fromId = 0,
+                        createdBy = 0,
+                        date = 1601510399,
+                        text = "this text post",
+                        replyOwnerId = 0,
+                        replyPostId = 0,
+                        friendsOnly = true,
+                        comments = Comments(count = 1, canPost = true, groupCanPost = true, canClose = false, canOpen = false),
+                        copyright = "copyright1",
+                        likes = Likes(count = 1, userLikes = true, canLike = false, canPublish = false),
+                        reposts = Reposts(count = 1, userReposted = false),
+                        views = Views(1),
+                        postType = "post",
+                        postSource = null,
+                        attachments = null,
+                        geo = null,
+                        signerId = 0,
+                        canPin = true,
+                        canDelete = true,
+                        canEdit = true,
+                        isPinned = true,
+                        markedAsAds = true,
+                        isFavorite = true,
+                        postponedId = 1
+                )
+        )
+
+        val updatePost = Post(
+                id = 45,
                 ownerId = 0,
                 fromId = 0,
                 createdBy = 0,
-                date = 1601510399,
-                text = "this text post",
+                date = 1601510500,
+                text = "this update text post",
                 replyOwnerId = 0,
                 replyPostId = 0,
                 friendsOnly = true,
@@ -121,6 +154,7 @@ class WallServiceTest {
                 views = Views(1),
                 postType = "post",
                 postSource = null,
+                attachments = null,
                 geo = null,
                 signerId = 0,
                 canPin = true,
@@ -130,40 +164,9 @@ class WallServiceTest {
                 markedAsAds = true,
                 isFavorite = true,
                 postponedId = 1
-            )
-        )
-
-        val updatePost = Post(
-            id = 45,
-            ownerId = 0,
-            fromId = 0,
-            createdBy = 0,
-            date = 1601510500,
-            text = "this update text post",
-            replyOwnerId = 0,
-            replyPostId = 0,
-            friendsOnly = true,
-            comments = Comments(count = 1, canPost = true, groupCanPost = true, canClose = false, canOpen = false),
-            copyright = "copyright1",
-            likes = Likes(count = 1, userLikes = true, canLike = false, canPublish = false),
-            reposts = Reposts(count = 1, userReposted = false),
-            views = Views(1),
-            postType = "post",
-            postSource = null,
-            geo = null,
-            signerId = 0,
-            canPin = true,
-            canDelete = true,
-            canEdit = true,
-            isPinned = true,
-            markedAsAds = true,
-            isFavorite = true,
-            postponedId = 1
         )
         val result = service.update(updatePost)
         assertFalse("false", result)
     }
-
-
 
 }
